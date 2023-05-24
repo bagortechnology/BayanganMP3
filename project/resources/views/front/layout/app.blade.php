a5a8df69752cdb70ec5ec9468108d819cc7374cd
= <=>
2023-05-24 23:57:31 +0800

    add placeholder

--- project/resources/views/front/layout/app.blade.php
+++ project/resources/views/front/layout/app.blade.php
@@ -323,7 +323,7 @@
                             <form action="{{ route('subscriber_send_email') }}" method="post" class="form_subscribe_ajax">
                                 @csrf
                                 <div class="form-group">
-                                    <input type="text" name="email" class="form-control" style="border-radius: 15px; border-color: #E38B29; background-color: #1A5F7A">
+                                    <input type="text" name="email" placeholder="email" class="form-control" style="border-radius: 15px; border-color: #E38B29; background-color: #1A5F7A">
                                     <span class="text-danger error-text email_error"></span>
                                 </div>
                                 <div class="form-group">
