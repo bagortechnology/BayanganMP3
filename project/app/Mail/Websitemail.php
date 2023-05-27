812bee8cef57a41a1126af7103de3ea6478ae41c
= <=>
2023-05-27 23:11:56 +0800

    edit Websitemail

--- project/app/Mail/Websitemail.php
+++ project/app/Mail/Websitemail.php
@@ -12,22 +12,12 @@ class Websitemail extends Mailable
     use Queueable, SerializesModels;
     public $subject, $body;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
     public function __construct($subject, $body)
     {
         $this->subject = $subject;
         $this->body = $body;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
         return $this->view('email.email')->with([
