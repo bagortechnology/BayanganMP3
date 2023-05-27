52e681813a8cbfd9b9bee4c1d896fa710a9164ba
= <=>
2023-05-27 14:48:28 +0800

    updated FAQ

--- project/resources/views/front/faq.blade.php
+++ project/resources/views/front/faq.blade.php
@@ -14,9 +14,11 @@
 
 <div class="page-content">
     <div class="container">
+        <h3 class="display-4 lead fw-semibold" style="font-family: 'Segoe UI', Arial, sans-serif">Have booking questions?</h3>
+        <h4 class="display-4 lead">We have answers.</h4>
         <div class="row">
             <div class="col-md-12">
-                <div class="accordion" id="accordionExample">
+                <div class="accordion" id="faq">
                     @php
                     $i=0;
                     @endphp
@@ -25,12 +27,12 @@
                     $i++;
                     @endphp
                     <div class="accordion-item">
-                        <h2 class="accordion-header" id="heading{{ $i }}">
+                        <h2 class="accordion-header" id="heading{{ $i }}" style="font-family: 'Segoe UI', Arial, sans-serif">
                         <button class="accordion-button @if($i!=1) collapsed @endif" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $i }}" aria-expanded="@if($i==1) true @else false @endif" aria-controls="collapse{{ $i }}">
                             {{ $item->question }}
                         </button>
                         </h2>
-                        <div id="collapse{{ $i }}" class="accordion-collapse collapse @if($i==1) show @endif" aria-labelledby="heading{{ $i }}" data-bs-parent="#accordionExample">
+                        <div id="collapse{{ $i }}" class="accordion-collapse collapse @if($i==1) show @endif" aria-labelledby="heading{{ $i }}" data-bs-parent="#faq">
                             <div class="accordion-body">
                                 {!! $item->answer !!}
                             </div>
